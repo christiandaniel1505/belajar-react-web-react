57ae41e9b1159a042cfde2ba79e1a55bf06543ea
christiandaniel1505 <danielchristian1505@gmail.com>
2022-03-09 15:48:38 +0700

    push

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { render } from '@testing-library/react';
+import FormLogin from './FormLogin';
+import Image from './Image';
 
-const Hello = () =>{
-  return <p>hello</p>
-}
-//ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render( <
+    React.StrictMode >
+    <App / >
+    </React.StrictMode>,
+    document.getElementById('root')
+);
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+// class StateFullComponent extends React.Component {
+//     render() {
+//         return <p>State Full Component</p>
+//     }
+// }
+
+ReactDOM.render( < FormLogin / > , document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.unregister();
+reportWebVitals();
